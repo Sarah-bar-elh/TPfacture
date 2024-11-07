52c4754df7a1aa6b3d5b19fb6fd65ab43c283dca
Sarah-bar-elh <sarah.barbier73@gmail.com>
2024-11-07 12:36:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,33 @@ function facture(){
     var a = document.getElementById("prix1").value;
     var b = document.getElementById("quantite1").value;
     var c = a * b;
-    document.getElementById("resultat1").textContent = c || 0;  
+    document.getElementById("resultat1").textContent = c || 0;  // Affiche 0 si vide
 
     // Calcul pour la deuxième ligne
     var d = document.getElementById("prix2").value;
     var e = document.getElementById("quantite2").value;
     var f = d * e;
-    document.getElementById("resultat2").textContent = f || 0; 
+    document.getElementById("resultat2").textContent = f || 0;  
+
+       // Calcul pour la  troisième ligne
+    var d = document.getElementById("prix3").value;
+    var e = document.getElementById("quantite3").value;
+    var f = d * e;
+    document.getElementById("resultat3").textContent = f || 0;  
 }
 
+function somme() {
+    var a = parseFloat(document.getElementById("resultat1").textContent) || 0;
+    var b = parseFloat(document.getElementById("resultat2").textContent) || 0;
+
+    var c = a + b;
+
+    document.getElementById("resultat4").textContent = c.toFixed(2);
+}
+ 
 function reinitialiser() {
-  
+    // Réinitialisation de tous les champs de saisie
     document.querySelectorAll('input[type="text"], input[type="number"]').forEach(input => input.value = '');
-  
+    // Réinitialisation de tous les champs de résultat
     document.querySelectorAll('.result').forEach(cell => cell.textContent = '0');
 }
